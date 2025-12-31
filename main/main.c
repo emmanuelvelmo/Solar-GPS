02e6a1a8d8ead062cf33f74dc27f47e20ba1adb4
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-12-30 17:59:06 -0800

    Update main.c

---
+++ main/main.c
@@ -0,0 +1,9 @@
+#include <> //
+
+// PLACA Y MÓDULOS
+// ESPWROOM32 XX5R69 ← ILI9341: 5V, GND, G14 (SCLK), G13 (MOSI), G2 (DC), G15 (CS), G26 (RST), G21 (BLK)
+//                   ← X: X
+//                   ← X: X
+
+-
+
